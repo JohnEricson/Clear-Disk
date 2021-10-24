94b06d60e61e7a3f6c7e70c2e9a6c946d97d9ebf
John Ericson <946327+JohnEricson@users.noreply.github.com>
2021-10-24 12:26:12 +0200

    IMPR: Standardized on the names of custom configured Tixati download directories on all my computers now.

--- Clear-Disk.ps1
+++ Clear-Disk.ps1
@@ -112,7 +112,7 @@ Write-Host "Clear Maven cache."
 Remove-Item -Force -Recurse "$env:USERPROFILE\.m2\repository"
 
 Write-Host "Clear files in Downloads directory."
-Remove-Item -Path "$env:USERPROFILE\Downloads" -Exclude 'Downloads', 'Telegram Desktop', 'Tixati complete downloads' -Force -Recurse
+Remove-Item -Path "$env:USERPROFILE\Downloads" -Exclude 'Downloads', 'Telegram Desktop', 'Tixati complete', 'Tixati incomplete' -Force -Recurse
 
 Write-host "Disk Usage before and after cleanup" -foreground yellow
     $FreespaceAfter = (Get-WmiObject win32_logicaldisk -filter "DeviceID='C:'" | select Freespace).FreeSpace/1GB
